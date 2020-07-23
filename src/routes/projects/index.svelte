4eb81b92ee661cd2558d2ed654d8a25f4431fce5
Nhua Nhua <nhuan@Nhuas-MacBook-Pro.local>
2020-07-24 00:35:55 +0700

    projects page

---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,43 @@
+<section class="container pt-5">
+    <!-- Title -->
+    <h1 class="text-center">Your projects</h1>
+
+    <!-- Projects -->
+    <div class="mt-5 row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <a href="/projects" class="btn btn-primary">View tasks</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <a href="/projects" class="btn btn-primary">View tasks</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <a href="/projects" class="btn btn-primary">View tasks</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
